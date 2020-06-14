a06049c8e8618ffa83743e03e011a4ecdfd78a69
Gvan-Iovnov <60030514+Gvan-Iovnov@users.noreply.github.com>
2020-06-14 21:31:51 +0300

    installed prettier and eslint

--- index.js
+++ index.js
@@ -1,96 +1,108 @@
-let findBtn = document.querySelector('.selector-find');
-let nextBtn = document.querySelector('.selector-next');
-let prevBtn = document.querySelector('.selector-prev');
-let topBtn = document.querySelector('.nav-top');
-let bottomBtn = document.querySelector('.nav-bottom');
-let leftBtn = document.querySelector('.nav-left');
-let rightBtn = document.querySelector('.nav-right');
-let elArr = [];
+const findBySelectorButton = document.querySelector(".selector-find");
+const findNextButton = document.querySelector(".selector-next");
+const findPreviousButton = document.querySelector(".selector-prev");
+const findParentButton = document.querySelector(".nav-top");
+const findChildButton = document.querySelector(".nav-bottom");
+const findNextNeighbourButton = document.querySelector(".nav-left");
+const findPreviousNeighbourButton = document.querySelector(".nav-right");
+let arrayOfElements = [];
 let current = 0;
-let style = document.createElement('style');
-let target;
-let arrOfChildren = [];
-style.type = 'text/css';
-style.innerHTML = '.selected { outline: solid red 5px; background-color: lightblue; }';
-document.getElementsByTagName('head')[0].appendChild(style);
-function checkTarget(target){
-    if(target.getElementsByTagName('*').length>0) bottomBtn.removeAttribute('disabled');
-    else bottomBtn.setAttribute('disabled', 'disabled');
-    if(document.body === target){
-        rightBtn.setAttribute('disabled', 'disabled');
-        leftBtn.setAttribute('disabled', 'disabled');
-        topBtn.setAttribute('disabled', 'disabled');
-        return;
-    }
-    if(target.parentElement) topBtn.removeAttribute('disabled');
-    else topBtn.setAttribute('disabled', 'disabled');
-    arrOfChildren = Object.values(target.parentElement.children);
-    arrOfChildren = arrOfChildren.filter(e => e.tagName!='BR');
-    console.log(arrOfChildren)
-    if(arrOfChildren.indexOf(target)<arrOfChildren.length-1) rightBtn.removeAttribute('disabled');
-    else rightBtn.setAttribute('disabled', 'disabled');
-    if(arrOfChildren.indexOf(target)>0) leftBtn.removeAttribute('disabled');
-    else leftBtn.setAttribute('disabled', 'disabled');
+const style = document.createElement("style");
+let selectedElement;
+let arrayOfChildren = [];
+style.type = "text/css";
+style.innerHTML =
+  ".selected { outline: solid red 5px; background-color: lightblue; }";
+document.getElementsByTagName("head")[0].appendChild(style);
+function examineSelectedElementElement(examinatedElement) {
+  if (examinatedElement.getElementsByTagName("*").length > 0)
+    findChildButton.removeAttribute("disabled");
+  else findChildButton.setAttribute("disabled", "disabled");
+  if (document.body === examinatedElement) {
+    findPreviousNeighbourButton.setAttribute("disabled", "disabled");
+    findNextNeighbourButton.setAttribute("disabled", "disabled");
+    findParentButton.setAttribute("disabled", "disabled");
+    return;
+  }
+  if (selectedElement.parentElement)
+    findParentButton.removeAttribute("disabled");
+  else findParentButton.setAttribute("disabled", "disabled");
+  arrayOfChildren = Object.values(selectedElement.parentElement.children);
+  arrayOfChildren = arrayOfChildren.filter((child) => child.tagName !== "BR");
+  // console.log(arrayOfChildren);
+  if (arrayOfChildren.indexOf(selectedElement) < arrayOfChildren.length - 1)
+    findPreviousNeighbourButton.removeAttribute("disabled");
+  else findPreviousNeighbourButton.setAttribute("disabled", "disabled");
+  if (arrayOfChildren.indexOf(selectedElement) > 0)
+    findNextNeighbourButton.removeAttribute("disabled");
+  else findNextNeighbourButton.setAttribute("disabled", "disabled");
 }
-document.querySelector('.jsbursa-panel').addEventListener("click", function(){
-    if(event.target == findBtn){
-        current = 0;
-        topBtn.setAttribute('disabled', 'disabled');
-        leftBtn.setAttribute('disabled', 'disabled');
-        rightBtn.setAttribute('disabled', 'disabled');
-        bottomBtn.setAttribute('disabled', 'disabled');
-        prevBtn.setAttribute('disabled', 'disabled');
-        nextBtn.setAttribute('disabled', 'disabled');
-        if(target) target.classList.remove('selected');
-        for (el of elArr) el.classList.remove('selected');
-        let s = document.querySelector('.selector').value;
-        elArr = document.querySelectorAll(s);
-        if(elArr.length==0) return;
-        console.log(elArr);
-        elArr[0].classList.add('selected');
-        target = elArr[0];
-        if(elArr.length>1){
-           nextBtn.removeAttribute('disabled');
-        }
-        checkTarget(target);
-        return;
+document
+  .querySelector(".jsbursa-panel")
+  .addEventListener("click", function panelClicked(event) {
+    if (event.target === findBySelectorButton) {
+      current = 0;
+      findParentButton.setAttribute("disabled", "disabled");
+      findNextNeighbourButton.setAttribute("disabled", "disabled");
+      findPreviousNeighbourButton.setAttribute("disabled", "disabled");
+      findChildButton.setAttribute("disabled", "disabled");
+      findPreviousButton.setAttribute("disabled", "disabled");
+      findNextButton.setAttribute("disabled", "disabled");
+      if (selectedElement) selectedElement.classList.remove("selected");
+      // for (el of arrayOfElements) el.classList.remove("selected");
+      if (Array.isArray(arrayOfElements))
+        arrayOfElements.map((element) => element.classList.remove("selected"));
+      const s = document.querySelector(".selector").value;
+      arrayOfElements = document.querySelectorAll(s);
+      if (arrayOfElements.length === 0) return;
+      // console.log(arrayOfElements);
+      arrayOfElements[0].classList.add("selected");
+      [selectedElement] = arrayOfElements;
+      if (arrayOfElements.length > 1) {
+        findNextButton.removeAttribute("disabled");
+      }
+      examineSelectedElementElement(selectedElement);
+      return;
     }
-    if(event.target == prevBtn){
-        nextBtn.removeAttribute('disabled');
-        elArr[current].classList.remove('selected');
-        current--;
-        elArr[current].classList.add('selected');
-        target = elArr[current];
-        if(current<1) prevBtn.setAttribute('disabled', 'disabled');
-        checkTarget(target);
-        return;
+    if (event.target === findPreviousButton) {
+      findNextButton.removeAttribute("disabled");
+      arrayOfElements[current].classList.remove("selected");
+      current -= 1;
+      arrayOfElements[current].classList.add("selected");
+      selectedElement = arrayOfElements[current];
+      if (current < 1) findPreviousButton.setAttribute("disabled", "disabled");
+      examineSelectedElementElement(selectedElement);
+      return;
     }
-    if(event.target == nextBtn){
-        prevBtn.removeAttribute('disabled');
-        elArr[current].classList.remove('selected');
-        current++;
-        elArr[current].classList.add('selected');
-        target = elArr[current];
-        if(current>=elArr.length-1) nextBtn.setAttribute('disabled', 'disabled');
-        checkTarget(target);
-        return;
+    if (event.target === findNextButton) {
+      findPreviousButton.removeAttribute("disabled");
+      arrayOfElements[current].classList.remove("selected");
+      current += 1;
+      arrayOfElements[current].classList.add("selected");
+      selectedElement = arrayOfElements[current];
+      if (current >= arrayOfElements.length - 1)
+        findNextButton.setAttribute("disabled", "disabled");
+      examineSelectedElementElement(selectedElement);
+      return;
     }
-    if(event.target.classList[0].includes('nav-')){
-        nextBtn.setAttribute('disabled', 'disabled');
-        prevBtn.setAttribute('disabled', 'disabled');
-        target.classList.remove('selected');
-    }else return;
-    if(event.target == topBtn){
-        target = target.parentElement;
-    }else if(event.target == bottomBtn){
-        target = target.children[0];
-    }else if(event.target == leftBtn){
-        target = arrOfChildren[arrOfChildren.indexOf(target)-1];
-        target.classList.add('selected');
-    }else if(event.target == rightBtn){
-        target = arrOfChildren[arrOfChildren.indexOf(target)+1];
-        target.classList.add('selected');
+    if (event.target.classList[0].includes("nav-")) {
+      findNextButton.setAttribute("disabled", "disabled");
+      findPreviousButton.setAttribute("disabled", "disabled");
+      selectedElement.classList.remove("selected");
+    } else return;
+    if (event.target === findParentButton) {
+      selectedElement = selectedElement.parentElement;
+    } else if (event.target === findChildButton) {
+      [selectedElement] = selectedElement.children;
+    } else if (event.target === findNextNeighbourButton) {
+      selectedElement =
+        arrayOfChildren[arrayOfChildren.indexOf(selectedElement) - 1];
+      selectedElement.classList.add("selected");
+    } else if (event.target === findPreviousNeighbourButton) {
+      selectedElement =
+        arrayOfChildren[arrayOfChildren.indexOf(selectedElement) + 1];
+      selectedElement.classList.add("selected");
     }
-    target.classList.add('selected');
-    checkTarget(target);
-});
+    selectedElement.classList.add("selected");
+    examineSelectedElementElement(selectedElement);
+  });
